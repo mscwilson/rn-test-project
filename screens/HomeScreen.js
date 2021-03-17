fab8a2fa23d9080f3f2dc9acf1cfa1c504088d9b
Miranda Wilson <mirandascwilson@gmail.com>
2021-03-17 14:58:43 +0000

    Add a second screen for results plus navigation to it

---
+++ screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>This is the Home Screen</Text>
+      <Button
+        title="Go to Results"
+        onPress={() => navigation.navigate("Results")}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 100
+  }
+});
+
+export default HomeScreen;
---
+++ screens/ResultsScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const ResultsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>This is the Results Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 100
+  }
+});
+
+export default ResultsScreen;
